14a350594c4c5e71b695256ba984ae12aa2b6c89
Champlain N. Bakop <champlainmarius20@gmail.com>
2022-05-26 14:20:33 -0400

    Add api

--- src/main/java/com/bixterprise/openshift/OpenshiftApplication.java
+++ src/main/java/com/bixterprise/openshift/OpenshiftApplication.java
@@ -1,11 +1,25 @@
 package com.bixterprise.openshift;
 
+import java.util.HashMap;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/api/hello")
 public class OpenshiftApplication {
 
+	@GetMapping("/{name}")
+	public HashMap<String, String> hello(@PathVariable("name") String name){
+		HashMap<String, String> re = new HashMap<>();
+		re.put("Say", "Hello "+name);
+		return re;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(OpenshiftApplication.class, args);
 	}
